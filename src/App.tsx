91a25eefbb087235066fc871a60504cbcce0dac1
maziar parsi <mparsi1992@gmail.com>
2024-07-08 15:57:42 +0330

    feat: add shuffle card functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,61 @@
+import { useState } from "react";
 import "./App.css";
 import { Button } from "./components";
 
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+
 function App() {
-  // const [count, setCount] = useState(0)
+  const repeatCardInGame = 2;
+
+  const [finalCards, setFinalCards] = useState(cardImages);
+  // create cards based on duplicate value we want
+  function createCardsList() {
+    let newUnorderedCards = cardImages;
+    for (let index = 1; index < repeatCardInGame; index++) {
+      newUnorderedCards = [...newUnorderedCards, ...cardImages];
+    }
+    return newUnorderedCards;
+  }
+
+  // we shuffle the cards list
+  function shuffleCards(list: { src: string }[]) {
+    let currentIndex = list.length;
+    const shuffledList = [];
+    const usedIndexes: number[] = [];
+    while (currentIndex != 0) {
+      const randomIndex = Math.floor(Math.random() * list.length);
+      if (!usedIndexes.includes(randomIndex)) {
+        usedIndexes.push(randomIndex);
+        shuffledList.push({...list[randomIndex],id:randomIndex+1});
+        currentIndex--;
+      }
+    }
+
+    // this is easier way to do this
+    // shuffledList=list.sort(()=>Math.random() - 0.5).map((card,index)=>({...card,id:index}))
+
+    return shuffledList
+  }
+
+  function handleClick() {
+    const unOrderedCards = createCardsList();
+    const finalList=shuffleCards(unOrderedCards);
+    setFinalCards(finalList)
+  }
+  console.log("finalCards",finalCards)
 
   return (
     <div className="bg-primary text-white h-screen w-screen flex justify-center py-8">
       <div>
         <h1 className="font-bold text-xl"> simple memory card</h1>
-        <Button>click me</Button>
+        <Button onClick={handleClick}>Start game</Button>
       </div>
     </div>
   );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,15 @@ import { FC } from "react";
 
 type TProps = {
   children: string;
+  onClick: () => void;
 };
 
-const Button: FC<TProps> = ({ children }) => {
+const Button: FC<TProps> = ({ children, onClick }) => {
   return (
-    <div className="hover:bg-btnHover w-52 bg-blue-500 border border-solid  border-white py-2 px-3 rounded-md text-white cursor-pointer text-center">
+    <div
+      onClick={onClick}
+      className="hover:bg-btnHover w-52 bg-blue-500 border border-solid  border-white py-2 px-3 rounded-md text-white cursor-pointer text-center"
+    >
       {children}
     </div>
   );
